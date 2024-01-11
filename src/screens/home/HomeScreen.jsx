dd15f46cbe541a87a66a3decb6e23b473e3279ae
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-01-10 21:38:34 -0500

    Add a basic counter with useState

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
+
 function HomeScreen() {
+
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <p>Hola mundo desde HomeScreen</p>
+      <p>Contador está en: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
     </>
   )
 }
